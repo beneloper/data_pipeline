2319fe785b0fd2da16bff43308b655475f6308e1
Ben L <beneloper@gmail.com>
2025-06-01 17:49:56 -0700

    Implemented APIs and completed the pipeline flow

--- app/data_source.py
+++ app/data_source.py
@@ -1,22 +1,14 @@
-import json
+import asyncio
 import random
-import time
 import datetime
+import uuid
 
-def generate_record(id: int):
-    return {
-        "timestamp": datetime.datetime.now().timestamp(),
-        "id": id, 
-        "value": round(random.uniform(10.0, 100.0), 2)
-    }
-
-def stream_data(interval=1.0, total_records=100):
-    record_count = 1
-    while record_count <= total_records:
-        record = generate_record(record_count)
-        print(json.dumps(record))  # Simulating real-time output
-        time.sleep(interval)
-        record_count += 1
-
-if __name__ == "__main__":
-    stream_data(interval=2, total_records=50)
+async def stream_data(queue, interval: int):
+    while True:
+        data = {
+            "timestamp": datetime.datetime.now(),
+            "id": uuid.uuid4(), 
+            "value": round(random.uniform(10.0, 100.0), 2)
+        }
+        await queue.put(data)
+        await asyncio.sleep(interval) # simulate n-seconds intervals
--- app/main.py
+++ app/main.py
@@ -1,15 +1,54 @@
-from fastapi import FastAPI
+import asyncio
+from typing import Annotated
+from fastapi import FastAPI, Query
+import pandas as pd
+
+from models import QueryFilter
+from consume_data import consume_data
+from data_source import stream_data
 
 app = FastAPI()
+data_queue = asyncio.Queue()
+results = {}
 
 @app.get('/trigger')
 async def trigger():
+    asyncio.create_task(stream_data(data_queue, 1))
+    asyncio.create_task(consume_data(data_queue, results))
     return {"message": "Data pipeline was triggered."}
 
 @app.get('/latest')
-async def get_latest():
-    return {"result": []}
+def get_latest():
+    return results['latest']
+
+@app.get('/melted')
+def get_melted():
+    return results['melted']
+
+@app.get('/pivoted')
+def get_pivoted():
+    return results['pivoted']
 
 @app.get('/query')
-async def query_data():
-    return {"result": []}
+async def query_data(query_filter: Annotated[QueryFilter, Query()]):
+    start = query_filter.start
+    end = query_filter.end
+
+    if start == None and end == None:
+        return get_latest()
+    
+    df = pd.DataFrame(results.get("latest", []))
+    if df.empty:
+        return []
+
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+
+    if start and end:
+        mask = (df['timestamp'] >= pd.to_datetime(start)) & (df['timestamp'] <= pd.to_datetime(end))
+    elif start:
+        mask = df['timestamp'] >= pd.to_datetime(start)
+    else:
+        mask = df['timestamp'] <= pd.to_datetime(end)
+
+    return df[mask].to_dict(orient='records')
